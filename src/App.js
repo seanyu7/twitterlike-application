63faae6649f90aefc22bd618647bc1fa0fc74357
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-12 21:55:16 +0900

    🎉 started making twitterlike applicattion

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        making react application
         </a>
       </header>
     </div>
