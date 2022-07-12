f04c05f06f52edfbb94e31543da9988b9fa7b80c
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-13 00:38:22 +0900

    ⚡️ install sidedar components from component folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
+import Sidebar from "./components /sidebar/Sidebar";
 
 function App() {
 
   const list = [1,2,3,4];
   
   return <div className="App">
-    <h1>三項演算子</h1>
-    {false ? <h1>true</h1>:<h1>false</h1>}
+    <Sidebar/>
     </div>;
 }
 
