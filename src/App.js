ea2f986127fde8a693d41ce8593c0af462903a43
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-12 23:54:11 +0900

    practiced about maping in react and keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">こんにちは</div>;
+
+  const list = [1,2,3,4];
+  
+  return <div className="App">
+    <h1>map-practice</h1>
+    {list.map(item => <div>{item *2}</div>)}
+    </div>;
 }
 
 export default App;
