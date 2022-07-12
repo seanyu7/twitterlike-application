7272b6de01a1d1408b5f5f87e0717d2515dc78fc
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-13 01:15:34 +0900

    🎨 made a component and use props to make it readable

--- src/components /sidebar/Sidebar.jsx
+++ src/components /sidebar/Sidebar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from '@mui/icons-material/Search';
+import AddAlertIcon from '@mui/icons-material/AddAlert';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="sidebar">
+      {/*downloading twitter icon from material UI */}
+      <TwitterIcon className="twitterIcon" />
+      
+      
+      {/*making side bar options here */}
+      <SidebarOption text = "Home" Icon ={HomeIcon}/>
+      <SidebarOption text = "Topics" Icon ={SearchIcon}/>
+      <SidebarOption text = "Notification" Icon ={AddAlertIcon}/>
+      <SidebarOption text = "Message" Icon ={ChatBubbleOutlineIcon}/>
+      <SidebarOption text = "BookMark" Icon ={BookmarkIcon}/>
+      <SidebarOption text = "list" Icon ={FormatListBulletedIcon}/>
+      <SidebarOption text = "Profile" Icon ={AccountBoxIcon}/>
+      <SidebarOption text = "More..." Icon ={MoreHorizIcon}/>
+      {/*making tweet button */}
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
