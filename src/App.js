dd30240f5d8f096c93f04410260437deb2b46058
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-14 11:52:43 +0900

    💄 made some change to create my app better looking

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import Sidebar from "./components /sidebar/Sidebar";
 
 function App() {
-
-  const list = [1,2,3,4];
   
   return <div className="App">
     <Sidebar/>
+    
     </div>;
 }
 
