18cf36bf3a30ebc93256bcd02bdf34b43884faa2
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-14 11:56:00 +0900

    ⚡️ inporting Timeline component from appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Sidebar from "./components /sidebar/Sidebar";
+import Timeline from "./components /timeline/Timeline";
 
 function App() {
   
   return <div className="App">
     <Sidebar/>
-    
+    {/*timeline*/}
+    <Timeline/>
+    {/*widgets*/ }
     </div>;
 }
 
