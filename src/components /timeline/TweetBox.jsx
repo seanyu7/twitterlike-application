81cbcdedb78e0f42adcf7f92bca52da8de4b0243
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-16 17:49:52 +0900

    💄 importing some material icon from materialUI

--- src/components /timeline/TweetBox.jsx
+++ src/components /timeline/TweetBox.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./tweetBox.css";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Button } from "@mui/material";
+import { Avatar, Button } from "@mui/material";
 
 function TweetBox() {
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBoxInput">
-        <AccountCircleIcon />
-        <input type="text" placeholder="what are you up to?" />
+          <Avatar />
+          <input type="text" placeholder="what are you up to?" />
         </div>
         <input
           className="tweetBoxImageInput"
