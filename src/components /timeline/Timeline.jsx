a7965440a31f568a6b85a1dc7026a22ed4951f50
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-16 16:39:04 +0900

    ⚡️ made a timeline component to make a timeline

--- src/components /timeline/Timeline.jsx
+++ src/components /timeline/Timeline.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./timeline.css";
+import TweetBox from "./TweetBox";
 
 function Timeline() {
   return (
-    <div className='timeline'>
-{/* header*/}
-<div className="timelineHeader">
-  <h2>Home</h2>
-</div>
-{/* Tweetbox*/}
-
-{/* Post*/}
-
+    <div className="timeline">
+      {/* header*/}
+      <div className="timelineHeader">
+        <h2>Home</h2>
+      </div>
+      {/* Tweetbox*/}
+      <TweetBox />
+      {/* Post*/}
     </div>
-  )
+  );
 }
 
-export default Timeline
+export default Timeline;
