e03aba57610adbf951ba815f910238c6294b863d
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-16 21:49:41 +0900

    ⚡️ made a widgets component to show widgets in app

---
+++ src/components /widget/Widgets.jsx
@@ -0,0 +1,20 @@
+import Search from "@mui/icons-material/Search";
+import React from "react";
+
+function Widgets() {
+  return (
+    <div className="widgets">
+      <div className="widgetsInput">
+        <Search className="widgetsSearchIcon" />
+        <input type="text" placeholder="Search with Keywords..." />
+      </div>
+      <div className="widgetsWidgetsContainer">
+        <h2>What are you up to? </h2>
+        {/*add some library*/}
+        
+      </div>
+    </div>
+  );
+}
+
+export default Widgets;
