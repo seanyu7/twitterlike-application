83caec6e6db6c8afb8a041f0004b31286d6de101
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-17 17:47:47 +0900

    ✨ made firebase.js to make use of database

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDHP7KuZIK8erf7cl5j9D63dfEk98A3ynE",
+    authDomain: "twitterlike-application.firebaseapp.com",
+    projectId: "twitterlike-application",
+    storageBucket: "twitterlike-application.appspot.com",
+    messagingSenderId: "446653385151",
+    appId: "1:446653385151:web:84aa9134bda5b26007390a"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
