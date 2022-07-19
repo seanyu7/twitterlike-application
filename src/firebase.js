14b56ce7034d884c8237956775da5dd652569bcc
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-19 21:54:42 +0900

    make use of firestore in this app

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHP7KuZIK8erf7cl5j9D63dfEk98A3ynE",
-    authDomain: "twitterlike-application.firebaseapp.com",
-    projectId: "twitterlike-application",
-    storageBucket: "twitterlike-application.appspot.com",
-    messagingSenderId: "446653385151",
-    appId: "1:446653385151:web:84aa9134bda5b26007390a"
-  };
+  apiKey: "AIzaSyDHP7KuZIK8erf7cl5j9D63dfEk98A3ynE",
+  authDomain: "twitterlike-application.firebaseapp.com",
+  projectId: "twitterlike-application",
+  storageBucket: "twitterlike-application.appspot.com",
+  messagingSenderId: "446653385151",
+  appId: "1:446653385151:web:84aa9134bda5b26007390a",
+};
 
-
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+/* ここの文章ではファイアーストアをAPPにて使うという意味でgetfirestoreの括弧の中にappと書いてある */
