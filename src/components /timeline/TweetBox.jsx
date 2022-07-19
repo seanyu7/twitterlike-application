836d2db1beb2c872409bba31cfb7a6c6d00f9dc5
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-19 22:41:17 +0900

    ⚡️ made a connection with firebase to put some data

--- src/components /timeline/TweetBox.jsx
+++ src/components /timeline/TweetBox.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import "./tweetBox.css";
 import { Avatar, Button } from "@mui/material";
+import { useState } from "react";
 
 function TweetBox() {
+  const [tweetMesage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    /*firebaseにデータにデータを追加する。*/
+    e.preventDefault();/*TWEETボタンを押してもページをリロードしない*/
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBoxInput">
           <Avatar />
-          <input type="text" placeholder="what are you up to?" />
+          <input
+            type="text"
+            placeholder="what are you up to?"
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
         </div>
         <input
           className="tweetBoxImageInput"
           placeholder="Put Your Image URL Here"
           type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
         />
       </form>
-      <Button className="tweetBoxTweetButton" type="submit">
+      <Button className="tweetBoxTweetButton" type="submit" onClick={sendTweet}>
         Tweet
       </Button>
     </div>
