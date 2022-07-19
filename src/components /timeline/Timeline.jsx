38e4acb4b2f884115f4b476a697faef5901bcc02
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-07-20 00:15:57 +0900

    ⚡️ make the tweet looks more smoothly

--- src/components /timeline/Timeline.jsx
+++ src/components /timeline/Timeline.jsx
@@ -3,9 +3,16 @@ import Post from "./Post";
 import "./timeline.css";
 import TweetBox from "./TweetBox";
 import db from "../../firebase";
-import { collection, getDocs, query, orderBy, onSnapshot} from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 import { useState } from "react";
 import { useEffect } from "react";
+import FlipMove from "react-flip-move";
 
 function Timeline() {
   const [posts, setPosts] = useState([]);
@@ -15,7 +22,7 @@ function Timeline() {
     const q = query(postData, orderBy("timestamp", "desc"));
     /*firebaseのドキュメントにて、順番の並び替えを行うときはfirebaseのドキュメントにて並べ方について
     調べれば教えてくれる。*/
-    
+
     /*この書き方だとリアルタイムではない取得の方法になっていしまう。
     
     getDocs(q).then((querySnapshot) => {
@@ -26,7 +33,7 @@ function Timeline() {
     onSnapshot(q, (querySnapshot) => {
       setPosts(querySnapshot.docs.map((doc) => doc.data()));
     });
-  },[]);
+  }, []);
 
   return (
     <div className="timeline">
@@ -37,18 +44,19 @@ function Timeline() {
       {/* Tweetbox*/}
       <TweetBox />
       {/* Post*/}
-
-      {posts.map((post) => (
-        <Post
-          key={post.text}
-          displayName={post.displayName}
-          userName={post.userName}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            userName={post.userName}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
